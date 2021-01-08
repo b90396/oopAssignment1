b340e72116a918068d3c80908a4ec8dd147dd5b9
rslvhsg <68112227+rslvhsg@users.noreply.github.com>
2021-01-08 17:02:30 +0000

    ?

--- OOP Task 1a/OOP Task 1a/Source.cpp
+++ OOP Task 1a/OOP Task 1a/Source.cpp
@@ -128,34 +128,34 @@ int main()
 
                 for (int i = 0; i < game.enemies.size(); i++)
                 {
-                   
 
-                    if (game.enemies[i].getXPos() == 1 && !movedDown)
-                    {
-                        enemyDirection = 'D';
-                        movedDown = true;
-                        break;
-                        
-                    }
-                    else if (game.enemies[i].getXPos() == 1 && movedDown)
-                    {
-                        enemyDirection = 'R';
-                        movedDown = false;
-                        break;
-                    }
+                        if (game.enemies[i].getXPos() == 1 && !movedDown)
+                        {
+                            enemyDirection = 'D';
+                            movedDown = true;
+                            break;
+
+                        }
+                        else if (game.enemies[i].getXPos() == 1 && movedDown)
+                        {
+                            enemyDirection = 'R';
+                            movedDown = false;
+                            break;
+                        }
+
+                        if (game.enemies[i].getXPos() == 20 && !movedDown)
+                        {
+                            enemyDirection = 'D';
+                            movedDown = true;
+                            break;
+                        }
+                        else if (game.enemies[i].getXPos() == 20 && movedDown)
+                        {
+                            enemyDirection = 'L';
+                            movedDown = false;
+                            break;
+                        }
                     
-                    if (game.enemies[i].getXPos() == 20 && !movedDown)
-                    {
-                        enemyDirection = 'D';
-                        movedDown = true;
-                        break;
-                    }
-                    else if (game.enemies[i].getXPos() == 20 && movedDown)
-                    {
-                        enemyDirection = 'L';
-                        movedDown = false;
-                        break;
-                    }
 
 
                     //FIX STARTING POSITION AND THEN NUMBER AFTERWARDS SO ENEMIES START AT FAR LEFT OF THE GRID
