805c64ecc4440fddea9cf843f4ed8e8ff8ddfce2
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2021-01-10 17:15:33 +0000

    Added Comments May need more work, just basic comments at the moment

--- OOP Task 1a/OOP Task 1a/EnemyProjectile.cpp
+++ OOP Task 1a/OOP Task 1a/EnemyProjectile.cpp
@@ -1,5 +1,13 @@
 #include "EnemyProjectile.h"
 
+
+/* Simple Inheritance from Projectile class that differentiates how these projectiles interact with the environment.
+*  The Enemy Projectiles only move downwards not up the grid, this is incorporated using the Move function called for 
+*  all enemy projectiles in the source.cpp.
+*  The Encapsulation just returns the value hasHitPlayer or modifies it 
+*  (Specific to the enemy projectiles since they are the only projectiles that interact with the player),
+*  For use when the game is running.
+*/
 void EnemyProjectile::setHasHitPlayer()
 {
 	hasHitPlayer = true;
--- OOP Task 1a/OOP Task 1a/EnemyProjectile.h
+++ OOP Task 1a/OOP Task 1a/EnemyProjectile.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "Projectile.h"
+
+/* Header File for the enemy projectiles, Inherits from the projectiles class
+* to work the projectile contstructor properly, the files incorporates "using" the default and set contructors
+* used in the Projectile class.
+* Includes all public methods to be accessed in the Game and Source files and the private variable used in the projectile classes
+*/
 class EnemyProjectile : public Projectile
 {
 public:
