3f6102b87b33e74c2e5f57940d54e3ec69ae094e
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2021-01-10 17:36:05 +0000

    Comments and Cleanup of PlayerProjectile class

--- OOP Task 1a/OOP Task 1a/PlayerProjectile.cpp
+++ OOP Task 1a/OOP Task 1a/PlayerProjectile.cpp
@@ -1,5 +1,12 @@
 #include "PlayerProjectile.h"
 
+/* Simple Inheritance from Projectile class that differentiates how these projectiles interact with the environment.
+*  The player Projectiles only move  up the grid, this is incorporated using the MovePlayerProjectile function called for 
+*  all player projectiles in the source.cpp.
+*  The Encapsulation just returns the value hasHitEnemy(HHE) or modifies it 
+*  (Specific to the player projectiles since they are the only projectiles that interact with the enemies),
+*  For use when the game is running.
+*/
 void PlayerProjectile::setHasHitEnemy(bool HHE)
 {
 	hasHitEnemy = HHE;
--- OOP Task 1a/OOP Task 1a/PlayerProjectile.h
+++ OOP Task 1a/OOP Task 1a/PlayerProjectile.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "Projectile.h"
+/* Player Projectile Header File Inherits from the projectile class in the same way as the enemy projectile class,
+*  It uses the constructors from the projectile class (using) and 3 public functions, 2 that set and return the value 
+*  when an enemy is hit (specific to the player projectile) and one that moves the projectile up the screen since the player projectile
+*  moves up not down.
+*  The Private variable is for when the projectile hits an enemy and both functions encapsulate this value.
+*/
 class PlayerProjectile : public Projectile
 {
 public:
